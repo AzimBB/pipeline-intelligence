7064e0ce948ba96ad21dedc1b7601815fe583f01
AzimBB <150686593+AzimBB@users.noreply.github.com>
2026-04-03 17:30:32 +0500

    main: app.py updated with simulated dataset

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Pipeline Intelligence MVP 🚀")
+st.title("Pipeline Intelligence Dashboard 🚀")
+
+df = pd.read_csv("data/simulated.csv")
+
+st.write("Dataset Preview")
+st.dataframe(df.head())
